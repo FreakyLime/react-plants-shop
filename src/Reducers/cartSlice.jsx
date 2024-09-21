ac488d43024d35bdfe6cc397b418a04c191c667d
Levan Chikhladze <levan.chikhladze@hotmail.com>
2024-09-21 12:44:17 +0400

    Update Cart to use product IDs

--- src/Reducers/cartSlice.jsx
+++ src/Reducers/cartSlice.jsx
@@ -28,47 +28,42 @@ export const CartSlice = createSlice({
   initialState,
   reducers: {
     addItem: (state, action) => {
-      const { name, image, cost, sale } = action.payload;
-      const existingItem = state.items.find(item => item.name === name);
+      const { id, name, image, cost, sale } = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
 
       if (existingItem) {
         existingItem.quantity += 1;
       } else {
-        state.items.push({ name, image, cost, sale, quantity: 1 });
+        state.items.push({ id, name, image, cost, sale, quantity: 1 });
       }
 
       saveCartToLocalStorage(state.items);
     },
 
     removeItem: (state, action) => {
-      const itemName = action.payload;
-      console.log("Removing item:", itemName);
-      const updatedItems = state.items.filter(item => item.name !== itemName);
+      const itemId = action.payload;
+      const updatedItems = state.items.filter(item => item.id !== itemId);
 
       if (updatedItems.length === state.items.length) {
-        console.warn(`Item with name "${itemName}" not found.`);
+        console.warn(`Item with name "${itemId}" not found.`);
       } else {
-        console.log("Item removed successfully.");
         state.items = updatedItems;
         saveCartToLocalStorage(state.items);
       }
     },
 
     updateQuantity: (state, action) => {
-      const { name, quantity } = action.payload;
+      const { id, quantity } = action.payload;
 
       const quantityNumber = Number(quantity);
       if (isNaN(quantityNumber) || quantityNumber < 1) {
-        console.error("Invalid quantity value");
         return;
       }
 
-      const itemToUpdate = state.items.find(item => item.name === name);
+      const itemToUpdate = state.items.find(item => item.id === id);
       if (itemToUpdate) {
         itemToUpdate.quantity = quantityNumber;
         saveCartToLocalStorage(state.items);
-      } else {
-        console.warn(`Item with name "${name}" not found.`);
       }
     },
   },
